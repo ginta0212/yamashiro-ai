83d6f6b2fb76853afb0fa7498fa3c2f264399bac
ginta0212 <discord2525@outlook.jp>
2025-06-27 21:55:41 +0900

    Update server.js

--- server.js
+++ server.js
@@ -29,3 +29,8 @@ app.post("/ask", async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+app.get("/", (req, res) => {
+  res.send("Yamashiro AI API Server is running!");
+});
